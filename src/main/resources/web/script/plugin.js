34ee3970213c25b982483f942bc0a14ebcb0956b
Jörg Richter <jri@deepamehta.de>
2016-11-07 17:30:34 +0100

    Adapt to changed DM WebSockets API

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -5,7 +5,7 @@ dm4c.add_plugin("de.ancud.camunda.dm4", function() {
         console.log("*** User \"" + username + "\" logged in (session " + js.get_cookie("JSESSIONID") + ")");
         //
         dm4c.get_plugin("de.deepamehta.websockets").create_websocket(
-            "ws://localhost:8081", "de.ancud.camunda.dm4", processMessage
+            "de.ancud.camunda.dm4", processMessage
         );
     });
 
